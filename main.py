4294cb2362b8b23e2c9cb5d4e8b49bdf062aa469
Kix Panganiban <kixpanganiban@protonmail.com>
2023-10-16 18:28:29 +0800

    Refine code using custom chain

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ dotenv.load_dotenv()
 
 from langchain.chains import RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.llms import OpenAI
+from langchain.llms import OpenAIChat
 from langchain.text_splitter import TokenTextSplitter
 from langchain.document_loaders import JSONLoader
+from langchain.prompts import ChatPromptTemplate
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores import Chroma
 
 
@@ -24,7 +29,43 @@ def cache_jira(project_key="AL"):
         os.environ["JIRA_EMAIL"], os.environ["JIRA_API_TOKEN"]
     )
 
+    def flatten_jira_text(text):
+        """Flatten Jira text into a single string."""
+        result = ""
+        if "type" in text:
+            if text["type"] == "text":
+                result += text["text"] + " "
+            elif text["type"] == "listItem":
+                result += "- "
+            elif text["type"] == "inlineCard":
+                result += text["attrs"]["url"].split("/")[-1] + " "
+        if "content" in text:
+            for item in text["content"]:
+                result += flatten_jira_text(item)
+            if text["type"] == "paragraph":
+                result += "\n"
+        return result
+
+    def extract_text_from_description(description):
+        """Extract text from a Jira issue description."""
+        if not description:
+            return ""
+        return flatten_jira_text(description)
+
+    def extract_text_from_comments(comments):
+        """Extract text from Jira issue comments."""
+        result = ""
+        for comment in comments:
+            result += (
+                comment["author"]["displayName"]
+                + " said: "
+                + flatten_jira_text(comment["body"])
+                + "\n"
+            )
+        return result
+
     def remap_issue(issue):
+        """Remap a Jira issue into a simpler more compact format."""
         mapped = {
             "key": issue.get("key", ""),
             "summary": issue["fields"].get("summary", ""),
@@ -52,13 +93,23 @@ def cache_jira(project_key="AL"):
         if not issue["fields"]["description"] and not response.json()["comments"]:
             return None
 
-        mapped["text"] = {
-            "description": issue["fields"]["description"],
-            "comments": [
-                {"body": comment["body"], "author": comment["author"]["displayName"]}
-                for comment in response.json()["comments"]
-            ],
-        }
+        mapped[
+            "text"
+        ] = """
+        Summary: {summary}
+        --
+        Key: {key}
+        --
+        Description:
+        {description}
+        
+        Comments:
+        {comments}
+        """.format(
+            key=issue["key"],
+            description=extract_text_from_description(issue["fields"]["description"]),
+            comments=extract_text_from_comments(response.json()["comments"]),
+        )
         print(f"Downloaded {issue['key']} from Jira...")
         return mapped
 
@@ -91,7 +142,10 @@ def cache_jira(project_key="AL"):
 
 
 def build_vector_store():
+    """Build a vector store from the Jira issues."""
+
     def metadata_func(record, metadata):
+        """Save the mapped Jira issue as metadata, except for the text."""
         for key, value in record.items():
             if key == "text":
                 continue
@@ -108,21 +162,42 @@ def build_vector_store():
     data = loader.load()
     token_splitter = TokenTextSplitter("gpt2")
     texts = token_splitter.split_documents(data)
-    docsearch = Chroma.from_documents(texts, OpenAIEmbeddings(), persist_directory="jira_chroma")
+    docsearch = Chroma.from_documents(
+        texts, OpenAIEmbeddings(), persist_directory="jira_chroma"
+    )
     docsearch.persist()
-    # docsearch.save_local("jira")
+
 
 
 def ask_question(question):
-    # docsearch = Chroma.load_local("jira", OpenAIEmbeddings())
-    docsearch = Chroma(embedding_function=OpenAIEmbeddings(), persist_directory="jira_chroma")
-    qa = RetrievalQA.from_chain_type(
-        llm=OpenAI(model="gpt-3.5-turbo-instruct", temperature=0),
-        chain_type="map_reduce",
-        retriever=docsearch.as_retriever(search_type="mmr"),
-        verbose=True,
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma(embedding_function=embeddings, persist_directory="jira_chroma")
+
+    template = """
+    You are a helpful knowledge assistant that provides answers to questions using issues from Jira.
+    If you don't know that answer, say so. Do not make up an answer.
+    Important: Do not ask the user to consult people or documentation. Do not create hyperlinks.
+    Expand on your answers as much as possible.
+    At the end of your answer, always cite your sources using the Issue key like so: "Sources: [key 1, key 2, ...]"
+    Use these Jira issues as context:
+    
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    model = OpenAIChat(model="gpt-3.5-turbo-16k", temperature=0)
+    chain = (
+        {
+            "context": docsearch.as_retriever(search_type="mmr", lambda_mult=0),
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | model
+        | StrOutputParser()
     )
-    print(qa.run(question))
+
+    return chain.invoke(question)
 
 
 def main():
@@ -139,7 +214,7 @@ def main():
         pass
         # chat()
     elif args.command == "ask":
-        ask_question(args.question)
+        print(ask_question(args.question))
 
 
 if __name__ == "__main__":
